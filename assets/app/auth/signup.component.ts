fe8c093c8dc6995b8f18108ba9e20d8dee0cdb3e
joshgressman <joshgressman@gmail.com>
2017-06-22 13:33:05 -0500

    added form validation to signup form

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -5,23 +5,32 @@ import { User } from './user.model';
 
 @Component({
   selector: 'app-signup',
-  templateUrl: './signup.component.html'
+  templateUrl: './signup.component.html',
+  styles: [`
+    input.ng-invalid.ng-touched {
+    border: 1px solid red;
+    }
+    `]
 })
 
 export class SignupComponent implements OnInit {
   myForm: FormGroup;
   constructor(private authService: AuthService){}
 
+  onSubmit(){
+    
+  }
+
  //Initalive the for group upon load and create custom validation
  ngOnInit(){
   this.myForm = new FormGroup({
    username: new FormControl(null, Validators.required),
-   password: new FormControl(null, Validators.required),
+   password: new FormControl(null, Validators.minLength(6)),
    email: new FormControl(null, [
      Validators.required,
      Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
    ])
  });
  }
- 
+
 }
