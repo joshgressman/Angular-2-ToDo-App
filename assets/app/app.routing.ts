521fe5a4ef8643151072803c654890951ceb900c
joshgressman <joshgressman@gmail.com>
2017-06-22 13:10:31 -0500

    added sign in form, routing and changed header for the auth routes

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AddTaskComponent } from './add-task/add-task.component';
 import { ViewTaskComponent } from './view-task/view-task.component';
+import { AuthenticationComponent } from './auth/authentication.component';
 
 const APP_ROUTES: Routes = [
  {path: '', redirectTo: '/view', pathMatch: 'full'},
  {path: 'view', component: ViewTaskComponent},
  {path: 'add', component: AddTaskComponent},
+ {path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule'}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -3,7 +3,19 @@ import { AuthService } from './auth.service';
 
 @Component({
  selector: 'app-authentication',
- template: ``
+ template: `
+           <header class="row spacing">
+            <nav class="col-md-8 col-md-offset-2">
+             <ul class="nav nav-tabs">
+              <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
+
+             </ul>
+            </nav>
+           </header>
+           <div class="row spacing">
+            <router-outlet></router-outlet>
+           </div>
+ `
 })
 
 export class AuthenticationComponent {
